5284b627af24b6dd1a45cd9cdee26917e564934b
EvgeniScherbakov <sherbakov_ev@bk.ru>
2024-02-07 00:51:50 +0400

    Final version of test.

--- src/test/java/org/example/RegistrationTest.java
+++ src/test/java/org/example/RegistrationTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -14,14 +15,47 @@ public class RegistrationTest {
     public static void setup() {
         System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
         myDriver = new ChromeDriver();
-        myDriver.manage().window().fullscreen();
         myDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        myDriver.get(ConfProperties.getProperty("loginpage"));
+        myDriver.get(ConfProperties.getProperty("regpage"));
         regPage = new RegPage(myDriver);
     }
     @Test
     public void registrationTest() {
-        regPage.inputFirstName("Zhenya");
+        String name = "Zhenya";
+        String lastName = "Scherbakov";
+        String email = "myTestEmail@gmail.com";
+        String number = "8800353535";
+        String imageWay = "C:\\Users\\Eugene\\IdeaProjects\\MyUITest\\src\\test\\resources\\earth.jpg";
+        String address = "Pushkina №4";
+        regPage.inputFirstName(name);
+        regPage.inputLastName(lastName);
+        regPage.inputEmail(email);
+        String gender = regPage.chooseGender();
+        regPage.inputMobileNumber(number);
+        regPage.clickBirthDateField();
+        String birthText = regPage.chooseDate();
+        regPage.inputSubjects("E");
+        String subject = regPage.clickSubject();
+        regPage.inputImage(imageWay);
+        regPage.inputAddress(address);
+        String state = regPage.setState();
+        String city = regPage.setCity();
         regPage.clickSubmitBtn();
+        try {
+            // Задержка на 2 секунды (2000 миллисекунд)
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        regPage.checkCongratsText();
+        regPage.assertNames(name + " " + lastName);
+        regPage.assertEmail(email);
+        regPage.assertGender(gender);
+        regPage.assertMobile(number);
+        regPage.assertBirth(birthText);
+        regPage.assertSubjects(subject);
+        regPage.assertPicture(imageWay);
+        regPage.asserAddress(address);
+        regPage.assertStateAndCity(state + " " + city);
     }
 }
